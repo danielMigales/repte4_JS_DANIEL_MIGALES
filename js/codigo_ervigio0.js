9c3874f9dfc14809945a54820356fa5c9f6de47c
Daniel <daniel.migales.puertas@gmail.com>
2021-07-07 11:35:57 +0200

    v1.1.

--- js/codigo_ervigio0.js
+++ js/codigo_ervigio0.js
@@ -16,41 +16,44 @@ function teclado(e) {
 	}
 }
 
-/*El codi javaScript que hauràs d’afegir en aquest tres primers condicional s’ha d’escriure al fitxer “codigo_ervigio0.js” (que es troba a la carpeta js ).*/
-
-/*Així, al fitxer " act0_JAB.html / codigo_ervigio0.js " quan l’usuari premi el botó "Aceptar":*/
-
-/*1. SI el nom escrit en el primer <INPUT> és " ervigi " o " ardabasto " (que és el seu pare) , i en qualsevol
-cas té igual o més de 18 anys , s’accedeix al fitxer 1: ( act1_JAB .html ).
-
-També ha de funcionar amb els noms en majúscules (“ ERVIGI ” o “ ARDABASTO ”) o en format
-oració (“ Ervigi ” o “ Ardabasto ”).
-
-També ha de funcionar amb els noms en majúscules (“ ERVIGI ” o “ ARDABASTO ”) o en format
-oració (“ Ervigi ” o “ Ardabasto ”).
-
-Traducció resumida:
-
-S’accedeix a “ act1_JAB.html ” quan:
-... l’usuari és ervigi (o ERVIGI o Ervigi) i té 18 anys o més.
-... l’usuari és ardabasto (o ARDABASTO o Ardabasto) i té 18 anys o més.
-(Si per exemple el nom de l'usuari és "ervigi", però té menys de 18 anys, no podrà accedir i
-passarà al següent condicional).*/
-
-
 function acceso() {
 
-	let nombre = document.getElementById("nombre").value.toLowerCase();
+	let nombre = document.getElementById("nombre").value.toLowerCase(); //con el toLowerCase pasa todo lo escrito en el input a minusculas
 	let edad = Number(document.getElementById("edad").value);
 
+	//punto 1: cuando se llama ervigi o ardabasto y tiene igual o mas de 18 pasa a fichero act1. 
 	if ((nombre == "ervigi" || nombre == "ardabasto") && edad >= 18) {
 		// Se accede al apartado (fichero) que corresponda		
 		location.href = "act1_JAB.html";
-	}
-	else {
 		// o act2_JAB.html o act3_JAB.html (según el nombre y edad del usuario)
-		alert("Aun no tienes acceso");
 	}
+
+	//punto 2: cuando se llama ataulf o o ataülf y la edad esta entre 70 y 100 años pasa a fichero act3.
+	else if ((nombre == "ataulf" || nombre == "ataülf") && (edad >= 70 && edad <= 100)) {
+		location.href = "act3_JAB.html";
+	}
+
+	//punto 3: si se llama cixilona y tiene 18 o se llama recesvint y tiene entre 70 y 99 años se limpian todos los inputs.
+	else if (nombre == "cixilona" && edad == 18 || nombre == "recesvint" && (edad >= 70 && edad <= 99)) {
+		reset(); //al cumplirse estas condiciones llamo a la funcion reset que limpia los inputs
+	}
+
+	//punto 4: si el usuario deja vacio algun campo no se accede a nada, vaciandose los dos inputs y fijandose el cursor.
+	else if ((nombre == "" || edad == "")) {
+		reset(); //si estan vacios se llama a la funcion de limpiar inputs
+	}
+
+	//punto 3: Al no cumplirse las condiciones (cixilona y 18 o recesvint y 70-99) todo lo demas pasa aqui y accede a fichero act2.
+	else {
+		location.href = "act2_JAB.html";
+	}
+}
+
+//(punto 4) Funcion para limpiar los dos inputs y fijar cursor
+function reset() {
+	document.getElementById("nombre").value = "";
+	document.getElementById("edad").value = "";
+	document.getElementById("nombre").focus();
 }
 
 
